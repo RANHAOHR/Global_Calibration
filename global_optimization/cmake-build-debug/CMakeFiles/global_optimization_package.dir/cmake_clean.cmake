7d58a7efedee52a51f79abd952a8c688a2ee11c9
RANHAOHR <rxh349@case.edu>
2017-07-18 12:29:37 -0400

    add generate datasets function

--- global_optimization/cmake-build-debug/CMakeFiles/global_optimization_package.dir/cmake_clean.cmake
+++
@@ -1,5 +0,0 @@
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/global_optimization_package.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- global_optimization/cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ranhao/ros_ws/src/Global_Calibration/global_optimization
-# Build directory: /home/ranhao/ros_ws/src/Global_Calibration/global_optimization/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
