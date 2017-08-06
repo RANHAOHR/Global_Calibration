c780ca58cc49721ab654bee9b88245d16a540bde
RANHAOHR <rxh349@case.edu>
2017-08-06 14:29:15 -0400

    get new results

--- global_optimization/cmake-build-debug/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "".split(';') if "" != "" else []
-PROJECT_CATKIN_DEPENDS = "message_runtime;std_msgs;sensor_msgs;geometry_msgs;xform_utils;cwru_opencv_common;tool_model_optimization;cwru_davinci_interface;cwru_davinci_kinematics".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "global_optimization"
-PROJECT_SPACE_DIR = "/home/ranhao/ros_ws/src/Global_Calibration/global_optimization/cmake-build-debug/devel"
-PROJECT_VERSION = "0.0.0"
